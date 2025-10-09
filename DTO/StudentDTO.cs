f72a78afe4c161be429b5f08141a567c8a324380
Đoàn Văn Phúc <phucvandoan123@gmail.com>
2025-10-09 20:58:16 +0700

    xoa

--- DTO/StudentDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace thibackend.DTOs
-{
-    public class StudentDto
-    {
-        public int Id { get; set; }
-        public string FullName { get; set; }
-        public int Age { get; set; }
-    }
-}
